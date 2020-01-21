31949555825d447f8ebc32012a7ee5a30f0a2993
Elias <eeliasmaia@gmail.com>
2020-01-20 22:09:30 -0300

    Valores padrão

--- bundle.js
+++ bundle.js
@@ -1,55 +1,10 @@
 "use strict";
 
-var arr = [1, 2, 3, 4, 5, 6];
-/* Antes 
-const newArr = arr.map(function(item) {
-	return item * 2;
-});
-
-Só um parâmetro, pode fazer isso:
-
-const newArr = arr.map(item => {
-	return item * 2;
-});
-
-*/
-
-/* Quando a função não tem muito processamento, e só retorna algo, pode-se fazer isso: */
-
-var newArr = arr.map(function (item) {
-  return item * 2;
-});
-console.log(newArr);
-/* Isso é bom em relação à verbosidade, quando se usa função anônima */
-
-/* Criando funções com arrow function e constantes */
-
-var teste = function teste() {
-  return [1, 2, 3];
+var soma = function soma() {
+  var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 3;
+  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 6;
+  return a + b;
 };
 
-var teste1 = function teste1() {
-  return 'teste';
-};
-
-var teste2 = function teste2() {
-  nome: 'Diego';
-}; //não vai funcionar usar chaves para retornar objeto
-
-/* Deveria ser assim: 
-
-const teste = () => {
-	return { nome: 'Diego'}
-}; */
-
-/* Para retornar o objeto, tem que colocar o parentese em volta da chave */
-
-
-var teste3 = function teste3() {
-  return {
-    nome: 'Diego'
-  };
-};
-
-console.log(teste());
-console.log(teste3());
+console.log(soma(1));
+console.log(soma());
--- main.js
+++ main.js
@@ -1,38 +1,4 @@
-const arr = [1, 2, 3, 4, 5, 6]
+const soma = (a = 3, b = 6) => a + b;
 
-/* Antes 
-const newArr = arr.map(function(item) {
-	return item * 2;
-});
-
-Só um parâmetro, pode fazer isso:
-
-const newArr = arr.map(item => {
-	return item * 2;
-});
-
-*/
-
-/* Quando a função não tem muito processamento, e só retorna algo, pode-se fazer isso: */
-const newArr = arr.map(item => item * 2);
-
-console.log(newArr)
-
-/* Isso é bom em relação à verbosidade, quando se usa função anônima */
-
-/* Criando funções com arrow function e constantes */
-const teste = () => [1, 2, 3];
-const teste1 = () => 'teste';
-const teste2 = () => { nome: 'Diego'}; //não vai funcionar usar chaves para retornar objeto
-
-/* Deveria ser assim: 
-
-const teste = () => {
-	return { nome: 'Diego'}
-}; */
-
-/* Para retornar o objeto, tem que colocar o parentese em volta da chave */
-const teste3 = () => ({ nome: 'Diego' });
-
-console.log(teste());
-console.log(teste3());
+console.log(soma(1));
+console.log(soma());
