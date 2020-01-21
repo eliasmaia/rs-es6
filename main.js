1216131207c57f2c4dae5fd3f399b1cad6e1c8c5
Elias <eeliasmaia@gmail.com>
2020-01-20 22:41:43 -0300

    Operadores rest/spread

--- main.js
+++ main.js
@@ -1,20 +1,48 @@
 const usuario = {
 	nome: 'Elias',
 	idade: 25,
-	endereco: {
-		cidade: 'Teresina',
-		estado: 'Piaui',
-	},
+	empresa: 'dot'
 };
 
-const { nome, idade, endereco: { cidade} } = usuario;
+const { nome, ...resto } = usuario;
 
 console.log(nome);
-console.log(idade);
-console.log(cidade);
+console.log(resto);	
 
-function mostraNome({ nome, idade }){
-	console.log(nome, idade);
+const arr = [1, 2, 3, 4];
+
+const [ a , b, ...c ] = arr;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+function soma(a, b, c, d){
+	return a + b + c + d;
 }
 
-mostraNome(usuario);
+console.log(soma(1, 3, 4, 5));	
+
+function soma2(a, b, ...params){
+	return params.reduce((total, next) => total + next);
+}
+
+console.log(soma2(1, 3, 4, 5, 6, 7, 8));	
+
+
+//SPREAD
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+const arr3 = [ ...arr1, ...arr2 ];
+console.log(arr3)
+
+const usuario1 = {
+	nome: 'Jules',
+	idade: 26,
+	empresa: Resistance,
+};
+
+const usuario2 = { ...usuario1, nome: 'Frank Frink'};
+console.log(usuario2)
