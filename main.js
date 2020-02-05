8d4874bee490005639befb8ee18790f03bb613fe
Elias <eeliasmaia@gmail.com>
2020-02-04 23:01:26 -0300

    import/export

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-import { soma } from './functions';
+import soma, { sub } from './functions';
+//import * as funcoes from './functions';
 
+//import somaFunction from './soma';
+
+
+//console.log(funcoes.soma(1, 2));
 console.log(soma(1, 2));
+console.log(sub(2, 1));
