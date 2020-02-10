f1317d379968f0ec7d8f91c64503e0154c6a0f45
Elias <eeliasmaia@gmail.com>
2020-02-10 19:45:42 -0300

    Webpack dev server

--- main.js
+++
@@ -1,9 +0,0 @@
-import soma, { sub } from './functions';
-//import * as funcoes from './functions';
-
-//import somaFunction from './soma';
-
-
-//console.log(funcoes.soma(1, 2));
-console.log(soma(1, 2));
-console.log(sub(2, 1));
